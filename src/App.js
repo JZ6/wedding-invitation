0ade32427d44a094c80ddca6a53f97ab58c8273a
Jay Zuo <yjzuo6@gmail.com>
2025-03-08 19:34:58 -0500

    welcome guests

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import logo from "./logo.svg";
 import "./App.css";
+import {getGuestData, getWelcomeMessage } from "./guests";
 
 function App() {
+  const { search } = window.location;
+  const params = new URLSearchParams(search);
+  const guestParam = params.get("guest");
+  const guestData = getGuestData(guestParam);
+
+  const welcomeMessage = getWelcomeMessage(guestData);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.sajs</code> and assasave to reload.
-        </p>
+        <p>{welcomeMessage}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/guests/index.js
@@ -0,0 +1,21 @@
+import guestList from "./guests.json";
+
+export function getGuestData(guestName) {
+  if (guestList.hasOwnProperty(guestName)) {
+    return guestList[guestName];
+  }
+  return false;
+}
+
+export function getWelcomeMessage(guestData) {
+  let welcomeMessage =
+    "Hm, your invitation seems to be invalid, please reach out to Jay!";
+
+  if (guestData) {
+    welcomeMessage = `Hi ${guestData.names
+      .join(", ")
+      .replace(/, ([^,]*)$/, ", and $1")}`;
+  }
+
+  return welcomeMessage;
+}
